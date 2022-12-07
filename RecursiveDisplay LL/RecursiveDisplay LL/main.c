db42e396dabe804728683b43b706a9f435b18980
Zhan <87288278+Zhanyl3909@users.noreply.github.com>
2022-12-07 16:20:30 +0900

    Recursive Display Linked List

---
+++ RecursiveDisplay LL/RecursiveDisplay LL/main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//declare a struct
+struct Node {
+    int data;
+    struct Node *next;
+}*first = 0;
+
+//create
+void create(int A[], int n) {
+    int i;
+    struct Node *t, *last;
+    first = (struct Node*)malloc(sizeof(struct Node));
+    
+    first->data = A[0];
+    first->next = NULL;
+    last = first;
+    
+    for(i=1; i<n; i++) {
+        t = (struct Node*)malloc(sizeof(struct Node));
+        t->data = A[i];
+        t->next = NULL;
+        last->next = t;
+        last = t;
+    }
+}
+
+
+void Display (struct Node *p) {
+    if(p!=NULL) {
+        
+        printf("%d ", p->data);
+        Display(p->next);
+       
+    }
+}
+
+
+int main() {
+    int A[] = {2,3,0,7,1,99};
+    int len = (sizeof(A)/sizeof(int));
+    
+    create(A,len);
+    Display(first);
+    
+    
+    return 0;
+}
